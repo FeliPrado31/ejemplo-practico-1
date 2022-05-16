c56ca104db69af8b0ce105eaed5cb3ca2e4c27cd
Feli Prado <juan.cubillo@mercadolibre.com.co>
2022-05-16 11:49:32 -0500

    tarea uno

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Contador</h1>
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button onClick={() => setCount(count - 1)}> -1</button>
+    </div >
+  );
+}
+
+export default App;
